a322b144eca1e109d042b74e156f2524b38689bc
Han Hyeji <77146030+Hyeeji@users.noreply.github.com>
2021-05-04 16:27:32 +0900

    add : 파일 열기 이벤트 추가

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QApplication, QMainWindow, QAction, qApp, QToolBar
+from PyQt5.QtWidgets import QApplication, QApplication, QMainWindow, QAction, qApp, QToolBar,QLabel,QPushButton,QTextEdit,QLineEdit,QDialog, QFileDialog
 from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
@@ -25,6 +25,7 @@ class MainWindow(QMainWindow):
 
         ''' Actions '''
         openImgFileAction = QAction(QIcon('./icon_img/file.png'), '시작하기', self)
+        openImgFileAction.triggered.connect(self.fileOpen)
         folderAction = QAction(QIcon('./icon_img/folder.png'), 'folder', self)
         saveAction = QAction(QIcon('./icon_img/save.png'), 'save', self)
 
@@ -35,9 +36,46 @@ class MainWindow(QMainWindow):
         self.file_toolbar.addAction(folderAction)
         self.file_toolbar.addAction(saveAction)
 
-
         self.show()
 
+    def fileOpen(self):
+        '''Import file window'''
+        self.dialog = QDialog()
+        self.dialog.setWindowTitle('이미지 도면 인식 시작 하기')
+        self.dialog.setGeometry(300, 100, 600, 300)
+
+        self.dialog.label = QLabel('Source file', self.dialog)
+        self.dialog.label.move(20, 10)
+
+        '''Dot Button'''
+        self.dialog.dotBtn = QPushButton('...', self.dialog)
+        self.dialog.dotBtn.resize(50, 30)
+        self.dialog.dotBtn.move(520, 5)
+        self.dialog.dotBtn.clicked.connect(self.dotBtnClick)
+
+        '''File Path'''
+        self.dialog.source = QLineEdit(self.dialog)
+        self.dialog.source.resize(300, 20)
+        self.dialog.source.move(225, 10)
+        self.dialog.source.setReadOnly(True)
+        self.dialog.source.setPlaceholderText('파일 경로')
+
+        self.dialog.path = QTextEdit(self.dialog)
+        self.dialog.path.resize(550, 200)
+        self.dialog.path.move(20, 40)
+        self.dialog.path.append('File Path')
+
+        '''Confirm button'''
+        self.dialog.confirm = QPushButton('OK', self.dialog)
+        # self.dialog.confirm.resize(60, 40)
+        self.dialog.confirm.move(500, 250)
+
+        self.dialog.show()
+
+    def dotBtnClick(self):
+        FileOpen = QFileDialog.getOpenFileName(self, '열기', './')
+        self.dialog.source.setText(FileOpen[0])
+        self.dialog.path.append(FileOpen[0])
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
