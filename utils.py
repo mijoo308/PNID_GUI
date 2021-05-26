436b36ea0855dde2eb0161ad099f52ec5829fa5d
miju <mijoo308@gmail.com>
2021-05-26 16:41:42 +0900

    feat : xml로 저장 기능 추가
    + 바뀐 result xml 형식에 맞게 수정

--- utils.py
+++ utils.py
@@ -5,62 +5,66 @@ import numpy as np
 import xml.etree.ElementTree as ET
 
 
-def makeXML(Boxes):
+# Result xml 형식 기준
+def makeXML(Boxes, filename):
     root = ET.Element('annotation')
-    ET.SubElement(root, 'filename').text = '수정필요' # filename 수정필요
+    ET.SubElement(root, 'filename').text = filename
 
-    #TODO : 인덱스 수정 필요
+    # string, orientation, xmin, ymin, xmax, ymax, visible
     for box in Boxes: #per Box
-        visible = box[6]
-        if visible == False:
+        visible = box[-1]
+        if not visible:
             continue
 
-        xmin = box[0]
-        ymin = box[1]
-        xmax = box[2]
-        ymax = box[3]
-        string = box[4]
-        orientation = box[5]
+        string = box[0]
+        orientation = box[1]
+        xmin = box[2]
+        ymin = box[3]
+        xmax = box[4]
+        ymax = box[5]
+        type = box[6]
+
         symbol_object = ET.SubElement(root, 'symbol_object')
-        ET.SubElement(symbol_object, 'type').text = '수정필요' #수정필요
+        ET.SubElement(symbol_object, 'type').text = type
         ET.SubElement(symbol_object, 'class').text = string
         bndbox = ET.SubElement(symbol_object, 'bndbox')
-        ET.SubElement(bndbox, 'xmin').text = xmin
-        ET.SubElement(bndbox, 'ymin').text = ymin
-        ET.SubElement(bndbox, 'xmax').text = xmax
-        ET.SubElement(bndbox, 'ymax').text = ymax
-        ET.SubElement(symbol_object, 'degree').text = orientation
-        ET.SubElement(symbol_object, 'flip').text = 'n' # 수정필요할수도
-        ET.SubElement(symbol_object, 'etc').text = None # 수정필요할수도
+        ET.SubElement(bndbox, 'xmin').text = str(xmin)
+        ET.SubElement(bndbox, 'ymin').text = str(ymin)
+        ET.SubElement(bndbox, 'xmax').text = str(xmax)
+        ET.SubElement(bndbox, 'ymax').text = str(ymax)
+        ET.SubElement(symbol_object, 'degree').text = str(orientation)
+        ET.SubElement(symbol_object, 'flip').text = 'n'
+        ET.SubElement(symbol_object, 'etc').text = None
 
 
     tree = ET.ElementTree(root)
-    tree.write('test.xml')  # 수정필요
+    tree.write(filename + '_fixed.xml')
 
 
-def object_to_element(xml_obj, type):
-    if type == 'gt':
-        string = xml_obj[1].text
-        orientation = int(float(xml_obj[2].text))
-        xmin = int(xml_obj[3][0].text)
-        ymin = int(xml_obj[3][1].text)
-        xmax = int(xml_obj[3][2].text)
-        ymax = int(xml_obj[3][3].text)
+def object_to_element(xml_obj, xml_type):
 
-        return string, orientation, xmin, ymin, xmax, ymax
+    bndbox = xml_obj.find("bndbox")
 
-    elif type == 'res':
-        string = xml_obj[0].text
-        orientation = int(float(xml_obj[1].text))
-        xmin = int(xml_obj[2][0].text)
-        ymin = int(xml_obj[2][1].text)
-        xmax = int(xml_obj[2][2].text)
-        ymax = int(xml_obj[2][3].text)
+    string = None
+    orientation = None
+    type = None
+    if xml_type == 'gt':
+        string = xml_obj.find('string').text
+        orientation = int(float(xml_obj.find('orientation').text))
+    elif xml_type == 'res':
+        string = xml_obj.find('class').text
+        orientation = int(float(xml_obj.find('degree').text))
+        type = xml_obj.find('type').text
 
-        return string, orientation, xmin, ymin, xmax, ymax
+    xmin = int(bndbox[0].text)
+    ymin = int(bndbox[1].text)
+    xmax = int(bndbox[2].text)
+    ymax = int(bndbox[3].text)
+
+    return string, orientation, xmin, ymin, xmax, ymax, type
 
 
-def parseXML(xml_path, type):
+def parseXML(xml_path, xml_type):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     # size = root.find('size')  # GT에만 있음
@@ -68,19 +72,20 @@ def parseXML(xml_path, type):
     # height = int(size.find('height').text)
 
     result = []
+    object = None
 
-    for child in root.findall('object'):
+    if xml_type == 'gt':
+        object = 'object'
+    elif xml_type == 'res':
+        object = 'symbol_object'
+
+    for child in root.findall(object):
         visible = True
-        string, orientation, xmin, ymin, xmax, ymax = object_to_element(child, type)
-        result.append([string, orientation, xmin, ymin, xmax, ymax, visible]) # add Visible
+        string, orientation, xmin, ymin, xmax, ymax, type = object_to_element(child, xml_type)
+        result.append([string, orientation, xmin, ymin, xmax, ymax, type, visible])  # add Visible
 
-    result = np.array(result) # np array 로 변환
+    result = np.array(result)  # np array 로 변환
 
     return result
-
-# for test
-# path = r'C:\Users\master\Desktop\PNID_GUI\test_res.xml'
-# result = parseXML(path, 'res')
-# print(result)
 
 
